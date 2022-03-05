61c31f17b6829209f830caa9a98f510a7b9b4a6d
Humma Noshine <Hummanoshine@gmail.com>
2022-03-05 00:34:29 +0000

    tests wrking...need to fix html

--- develop/index.js
+++ develop/index.js
@@ -20,7 +20,7 @@ let teamArray= [];
     {
       type: "list",
       name: "options",
-      message:"Welcome! Please select one of the below to start building your team profile", 
+      message:"Hi There! Welcome to Team builder! What would you like to do? Select an option below..", 
       choices: ["Add a Manager", "Add an Engineer", "Add an Intern"],
     },
      {
@@ -62,7 +62,7 @@ let teamArray= [];
       message:"Would you like to add more employees to your team profile?",
     },
    ])
-  //  Bring in classes..if x task is selected then push answers..or generate html..
+  //  Bring in classes..if x task is selected then push answers.. generate html..
 
    .then ((answers) => {
      if (answers.options === "Add a Manager") {
@@ -96,12 +96,10 @@ const CreateStuff = () => {
    
 }
 
-// look at puis example and change...
-
 // CHAMGE THE HTML..................................................................
 
 const generateHTML = () => {
-return 
+ 
   `<!DOCTYPE html>
 <html lang="en">
   <head>
@@ -120,65 +118,76 @@ return
         <h1>Our Team</h1>
       </div>
     </div>
-      <div class="container">
+    </body>
+</html>`
+};
+
+    const CreateManager = mang => {
+      return `<div class="container">
+      <div class="card">
+      <div class="row text">
+        <div class="col-sm-4">
+          <div class="card-header">
+          <h2>${Manager.name}</h2>
+          <p class="lead">${Manager.getRole()}</p></div>
+          <h3><span class="badge badge-secondary">Contact Info</span>
+          </h3>
+          <ul class="list-group">
+            <li class="list-group-item">Email: <a href = "mailto:${Manager.email}</a></li>
+            <li class="list-group-item">Employee ID:   ${Manager.id}</li>
+            <li class="list-group-item">Office Number: ${Manager.getOfficeNumber()}</li>
+          </ul>
+        </div>
+        </div>
+      </div>
+    </div>
+  </div>`
+    };
+      const CreateEngineer = eng => {
+        `<div class="container" id="engineer">
         <div class="card">
         <div class="row text">
           <div class="col-sm-4">
             <div class="card-header">
-            <h2>${Employee.getRole}</h2>
-            <p class="lead">Manager</p></div>
+            <h2>${Engineer.name}</h2>
+            <p class="lead">${Engineer.getRole()}</p></div>
             <h3><span class="badge badge-secondary">Contact Info</span>
             </h3>
             <ul class="list-group">
-              <li class="list-group-item">Email:         ${Manager.getEmail}</li>
-              <li class="list-group-item">Employee ID:   ${Manager.getId}</li>
-              <li class="list-group-item">Office Number: ${Manager.getOfficeNumber}</li>
+              <li class="list-group-item">Email:  <a href = "mailto:${Engineer.email}</a></li>
+              <li class="list-group-item">Employee ID:   ${Engineer.id}</li>
+              <li class="list-group-item">GitHub Profile: <a href= "https://github.com/${Engineer.getGithub()}" target="_blank">${engineerGithub}</a></li>
             </ul>
           </div>
           </div>
         </div>
       </div>
-    </div>
-    <div class="container" id="engineer">
-    <div class="card">
-    <div class="row text">
-      <div class="col-sm-4">
-        <div class="card-header">
-        <h2>${enginName}</h2>
-        <p class="lead">Engineer</p></div>
-        <h3><span class="badge badge-secondary">Contact Info</span>
-        </h3>
-        <ul class="list-group">
-          <li class="list-group-item">Email:         ${engineerEmail}</li>
-          <li class="list-group-item">Employee ID:   ${engineerID}</li>
-          <li class="list-group-item">GitHub Profile: <a href= "https://github.com/${engineerGithub}" target="_blank">${engineerGithub}</a></li>
-        </ul>
-      </div>
-      </div>
-    </div>
-  </div>
-</div>
-<!-- Intern card -->
-<div class="container" id="intern">
-  <div class="card">
-  <div class="row text">
-    <div class="col-sm-4">
-      <div class="card-header">
-      <h2>undefined</h2>
-      <p class="lead">Intern</p></div>
-      <h3><span class="badge badge-secondary">Contact Info</span>
-      </h3>
-      <ul class="list-group">
-     
-        <li class="list-group-item">Email:         ${internEmail}</li>
-        <li class="list-group-item">Employee ID:   ${internID}</li>
-        <li class="list-group-item">School:         ${internSchool}</li>
-    </ul>
-    </div>
-    </div>
-  </div>
-</div>
-</div>
-  </body>
-</html>`
-};
+    </div>`
+      };
+
+      const CreateIntern = int => {
+        `<!-- Intern card -->
+        <div class="container" id="intern">
+          <div class="card">
+          <div class="row text">
+            <div class="col-sm-4">
+              <div class="card-header">
+              <h2>${Intern.name}</h2>
+              <p class="lead">${Intern.getRole()}</p></div>
+              <h3><span class="badge badge-secondary">Contact Info</span>
+              </h3>
+              <ul class="list-group">
+             
+                <li class="list-group-item">Email:  <a href = "mailto:${Intern.email}</a></li>
+                <li class="list-group-item">Employee ID:   ${Intern.id}</li>
+                <li class="list-group-item">School:         ${Intern.getSchool()}</li>
+            </ul>
+            </div>
+            </div>
+          </div>
+        </div>
+        </div>`
+      };
+    
+
+  
